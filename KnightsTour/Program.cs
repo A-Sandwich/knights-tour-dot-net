cca4c3c55b41835957418ad3a2eb7f9efbeb0430
Kyle <kbburkholder@gmail.com>
2025-05-04 09:38:05 -0400

    set encoding so my super cool emojis render 👍

--- KnightsTour/Program.cs
+++ KnightsTour/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text;
 using KnightsTour;
-
+Console.OutputEncoding=Encoding.UTF8;
 Console.WriteLine("Hello, World!");
 var manager = new BoardManager(64);
 manager.DepthFirstSearch();
